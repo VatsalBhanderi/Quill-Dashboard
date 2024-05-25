ab7dcd8e00813cc9d9e9bce24d63c5e22005b289
Vatsal Bhanderi <vbhanderi6@gmail.com>
2024-05-25 09:21:01 -0700

    Initializing Spring Boot project - Backend

---
+++ backend/quill/src/main/java/com/example/quill/QuillApplication.java
@@ -0,0 +1,13 @@
+package com.example.quill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuillApplication.class, args);
+	}
+
+}
---
+++ backend/quill/src/test/java/com/example/quill/QuillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
